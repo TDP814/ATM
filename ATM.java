6af55075edcd133e784b4bf9fe8fa28d17f323b0
TDP814 <Perkinstd@s.dcsdk12.org>
2013-10-10 09:15:25 -0600

    Code added

---
+++ ATM.java
@@ -0,0 +1,19 @@
+public class ATM {
+
+	private Atm[];
+	//State
+	//	Accounts
+
+	//Behavior
+	//	Login(String name, int pin)
+	//		Check Atm for name and pin
+	public void login(String name, int pin){
+		for (int i=0; i>=name.length; i++) {
+			
+		}
+	}
+
+	public void checkATM(String name, int pin){
+
+	}
+}

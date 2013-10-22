764c67df059ea793232c73b65a3b0a76892ec5fd
TDP814 <Perkinstd@s.dcsdk12.org>
2013-10-22 08:03:50 -0600

    Organized

--- ATM.java
+++ ATM.java
@@ -11,7 +11,6 @@ public class ATM {
 	private User account1 = new User("Bob", "1234", 100.00);
 	private User account2 = new User("Alice", "3333", 250.00);
 	private User account3 = new User("JT", "0001", 999999999.00);
-	private User blank = new User("Null", "Null", 0.00);
 	private User[] users = {account1, account2, account3};
 	//constructor
 	public ATM() {
@@ -36,31 +35,22 @@ public class ATM {
 			System.out.println();
 
 			for (int i = 0; i < users.length; i++) {
-
 				if (name.equals(users[i].name())) {
-
 					index = i;
 				}
 			}
 
 			if (index == -1) {
-
 				System.out.println("Invalid name");
-
 			} else {
 				System.out.print("PIN: ");
-
 				p = a.nextLine();
 				if (p.equals(users[index].PIN())) {
-
 					System.out.println("Login successful");
-
 				} else {
 					System.out.println("Login failed");
-
 					index = -1;
 				}
-
 			}
 		}
 		this.active = users[index];
@@ -70,39 +60,28 @@ public class ATM {
 	public void commands() {
 
 		System.out.println("Welcome " + active.name());
-
 		System.out.print("Type 1 to check balance, 2 to withdraw funds, 3 to change name or PIN, or 4 to logout: ");
-
 		String input = a.nextLine();
-
 		System.out.println();
 		if (input.equals("1")) {
-
 			checkBalance();
 		} else if (input.equals("2")) {
-
 			withdraw();
 		} else if (input.equals("3")) {
-
 			change();
 		} else if (input.equals("4")) {
-
 			logout();
 		} else {
 			System.out.println("Unknown command");
-
 			commands();
 		}
 	}
 
 	public void withDraw() {
 		System.out.print("Amount to withDraw: ");
-
 		int w = a.nextInt();
-
 		System.out.println();
 		active.setBalance(active.balance()- w);
-
 		commands();
 	}
 
@@ -111,12 +90,10 @@ public class ATM {
 
 	public void checkBalance() {
 		System.out.println("Current balance is: " + active.balance());
-
 		commands();
 	}
 
 	public void logout() {
-
 		active = blank;
 		System.out.println("Log out worked");
 
@@ -126,24 +103,17 @@ public class ATM {
 	public void change() {
 
 		System.out.print("Type 1 to change name, 2 to change PIN: ");
-
 		String input = a.nextLine();
 
 		if (input.equals("1")) {
-
 			System.out.println("New name: ");
-
 			String newname = a.nextLine();
-
 			System.out.println();
 			active.setName(newname);
 
 		} else if (input.equals("2")) {
-
 			System.out.println("New PIN: ");
-
 			String newPIN = a.nextLine();
-
 			System.out.println();
 			active.setPIN(newPIN);
 
